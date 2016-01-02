11feb623560bdeb23aab0264415cff692c4a8292
Navyjun <zhanghaijun@zhanghaijundeMacBook-Pro.local>
2016-01-03 00:10:40 +0800

    初始化
    初始化

---
+++ Music-imitate/Music-imitate/Classes/Other/System/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Music-imitate
+//
+//  Created by 张海军 on 16/1/2.
+//  Copyright © 2016年 navyzhj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
